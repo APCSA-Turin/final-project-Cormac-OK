304f6fa0818a9ea00ce275611efd30c2dddb1243
Cormac-OK <cormaco@nycstudents.net>
2025-05-21 12:14:56 +0000

    Push

--- JavaAPIProject/src/main/java/com/example/App.java
+++ JavaAPIProject/src/main/java/com/example/App.java
@@ -1,4 +1,7 @@
 package com.example;
+import java.util.ArrayList;
+
+import org.json.JSONArray;
 import org.json.JSONObject;
 /**
  * Hello world!
@@ -6,8 +9,22 @@ import org.json.JSONObject;
  */
 public class App 
 {
+
+    private static ArrayList<Image> images;
+
+
+    public static Image getRandomImage(){
+        return new Image(null, null, null, 0);
+    }
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        RandomImage rand = new RandomImage(2);
+        for(int i = 0; i < 17; i ++){
+            System.out.println(rand.randomImage());
+            System.out.println();
+        }
     }
+
+    
 }
